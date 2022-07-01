244f1541cb65f805431230d61caeab3792e9a34d
negin banay shahani <42892521+negin-shahani@users.noreply.github.com>
2022-07-01 17:37:10 +0430

    Update Day 0: Hello, World.js

--- Day 0: Hello, World.js
+++ Day 0: Hello, World.js
@@ -1,3 +1,9 @@
 ```js
-console.log(x);
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log(inputString);
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
+
 ```
